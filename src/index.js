f7a75575e1c2b3baa18aea5ab21cb39832506734
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-06-07 00:03:19 -0300

    adding react router to the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './index.css';
+
 import App from './App';
+import AutorBox from './autor/Autor';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    (<BrowserRouter >
+        <div>
+            <Switch>
+                <Route path='/' component={App} />
+                <Route path='/autor' component={AutorBox} />
+            </Switch>
+        </div>
+    </BrowserRouter>),
+    document.getElementById('root')
+);
 registerServiceWorker();
